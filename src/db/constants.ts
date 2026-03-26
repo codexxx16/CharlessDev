ae03e708058d264dda3ab0b7ada437fd0f6332b4
Charless Netumbare <codexxxnull@gmail.com>
2026-03-26 07:29:37 -0400

    chore: complete removal of database and auth related files

--- src/db/constants.ts
+++
@@ -1,3 +0,0 @@
-export const DEFAULT_SETTINGS = {
-  replyNotificationsEnabled: true,
-}
--- src/orpc/schemas/github.schema.ts
+++
@@ -1,8 +0,0 @@
-import * as z from 'zod'
-
-export const GithubStatsOutputSchema = z.object({
-  stars: z.number(),
-  followers: z.number(),
-  repoStars: z.number(),
-  publicRepos: z.number().optional(),
-})
