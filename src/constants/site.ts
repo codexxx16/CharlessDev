fbb355dc73528b57eea6609c7ed9f0cca174f05a
codexxx16 <206998241+codexxx16@users.noreply.github.com>
2026-04-02 07:20:27 -0400

    Phase 3: Update package.json and site constants

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,12 +1,16 @@
 export const SITE_GITHUB_URL = 'https://github.com/codexxx16'
-export const SITE_FACEBOOK_URL = 'https://www.facebook.com/codexxx16'
-export const SITE_INSTAGRAM_URL = 'https://www.instagram.com/codexxx16'
-export const SITE_X_URL = 'https://x.com/codexxx16'
-export const SITE_YOUTUBE_URL = 'https://www.youtube.com/@codexxx16'
+export const SITE_FACEBOOK_URL = ''
+export const SITE_INSTAGRAM_URL = ''
+export const SITE_X_URL = ''
+export const SITE_YOUTUBE_URL = ''
 
 export const GITHUB_USERNAME = 'codexxx16'
 
 export const MY_NAME = 'Charless Netumbare'
+export const SITE_NAME = 'CharlessDev'
+export const SITE_URL = 'https://charless-dev.vercel.app'
+export const SITE_DESCRIPTION = 'Full Stack Developer and AI Engineer from Masvingo, Zimbabwe'
+export const SITE_EMAIL = 'codexxxnull@gmail.com'
 
 export const OG_IMAGE_WIDTH = 1200
 export const OG_IMAGE_HEIGHT = 630
